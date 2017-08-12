ee4757c7558188654ad3954bac6057a6dc8cf01c
--replace <mazg1493@gmail.com>
2017-08-13 01:47:08 +0600

    Initial Database models

---
+++ plan/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Timestamp(models.Model):
+    created_at = models.DateTimeField(auto_now=True, blank=True, null=True)
+    updated_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+
+    class Meta:
+        abstract = True
+
+
+class Plan(Timestamp):
+    user = models.OneToOneField(User)
+    type = models.IntegerField(choices=((1, "Once"), (2, "Custom")))
+
+
+class PlanDetails(Timestamp):
+    plan = models.ForeignKey(Plan)
+    end_time = models.TimeField()
+    status = models.IntegerField(choices=((1, "Active"), (2, "Cancelled")))
+    sunday = models.NullBooleanField(default=False)
+    monday = models.NullBooleanField(default=False)
+    tuesday = models.NullBooleanField(default=False)
+    wednesday = models.NullBooleanField(default=False)
+    thursday = models.NullBooleanField(default=False)
+    friday = models.NullBooleanField(default=False)
+    saturday = models.NullBooleanField(default=False)
