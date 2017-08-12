43681b32c284f58b6bd439c8724b51bd5c9e457e
tm <mashrikt@gmail.com>
2017-08-13 00:52:19 +0600

    material design

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -145,13 +145,3 @@ STATICFILES_DIRS = (
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'lunchhobe',
-        'USER': 'tm2',
-        'PASSWORD': 'aaaa1111',
-        'HOST': 'localhost',
-        'PORT': '',
-    }
-}
