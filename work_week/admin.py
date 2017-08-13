0d18efd2f871b0ff2ebf9291733e3204d84bbee7
tm <mashrikt@gmail.com>
2017-08-14 02:47:50 +0600

    deleting Day disabled

--- work_week/admin.py
+++ work_week/admin.py
@@ -11,6 +11,9 @@ class DayAdmin(admin.ModelAdmin):
     def has_add_permission(self, request):
         return False
 
+    def has_delete_permission(self, request, obj=None):
+        return False
+
 
 # Register your models here.
 admin.site.register(Day, DayAdmin)
