8dda49d98722a0e6a0a54367851354342c237783
tm <mashrikt@gmail.com>
2017-08-14 01:50:15 +0600

    admin logo and title

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -66,7 +66,7 @@ ROOT_URLCONF = 'gettingstarted.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'debug': True,
