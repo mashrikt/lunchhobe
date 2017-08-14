27b2659655f93b51e97e774ef5199af8e51d052d
--replace <mazg1493@gmail.com>
2017-08-15 01:29:40 +0600

    Order calculation done against extra_orders

--- order_details/signals.py
+++ order_details/signals.py
@@ -8,13 +8,29 @@ from order_details.models import Order
 def post_save_order(sender, instance, created, **kwargs):
 
     previous_extra_orders = instance.get_dirty_fields().get('extra_orders')
+    previous_user_list = instance.get_dirty_fields().get('user')
+    # print(previous_user_list)
+    current_user_list = Order.objects.values_list('user', flat=True)
+    # print(current_user_list)
 
-    if not previous_extra_orders and instance.extra_orders != 0:
-        instance.total = (instance.price * instance.extra_orders) + instance.total
-        instance.save()
+    print(previous_extra_orders)
+    print(instance.extra_orders)
 
-    elif previous_extra_orders and instance.extra_orders != 0:
-        instance.total = (instance.total -
-                          (instance.price * previous_extra_orders) +
-                          (instance.price * instance.extra_orders))
-        instance.save()
+    if previous_extra_orders is not None:
+        # print("Change in extra orders")
+        if not previous_extra_orders and instance.extra_orders:
+            # print(instance.total)
+            # print(instance.price * instance.extra_orders)
+            # print(instance.total + (instance.price * instance.extra_orders))
+            instance.total = (instance.price * instance.extra_orders) + instance.total
+            Order.objects.filter(id=instance.id).update(total=instance.total, extra_orders=instance.extra_orders)
+
+        elif previous_extra_orders and instance.extra_orders:
+            # print(instance.total)
+            # print(instance.price * previous_extra_orders)
+            # print(instance.price * instance.extra_orders)
+            # print(instance.total + (instance.price * instance.extra_orders) -
+            #       (instance.price * previous_extra_orders))
+            instance.total = (instance.total + (instance.price * instance.extra_orders) -
+                              (instance.price * previous_extra_orders))
+            Order.objects.filter(id=instance.id).update(total=instance.total, extra_orders=instance.extra_orders)
