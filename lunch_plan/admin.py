e35328a6e0a74f8c8dc11d3970b03e43b5634506
tm <mashrikt@gmail.com>
2017-08-15 05:16:41 +0600

    save_model if user is not superuser for saving user in Daily and Weekly Plan Admin

--- lunch_plan/admin.py
+++ lunch_plan/admin.py
@@ -5,12 +5,16 @@ from .models import DailyPlan, WeeklyPlan
 
 
 class DailyPlanAdmin(admin.ModelAdmin):
-    list_display = ('date', 'day', 'user', 'will_have_lunch')
     icon = '<i class="material-icons">restaurant</i>'
-
+    exclude = []
+    list_display = []
+    
     def changelist_view(self, request, extra_context=None):
+        self.list_display = []
         if not request.user.is_superuser:
-            self.list_display = ('date', 'day', 'will_have_lunch')
+            self.list_display = ['date', 'day', 'will_have_lunch']
+        else:
+            self.list_display = ['date', 'day', 'user', 'will_have_lunch']
         return super(DailyPlanAdmin, self).changelist_view(request, extra_context)
 
     def get_queryset(self, request):
@@ -19,14 +23,29 @@ class DailyPlanAdmin(admin.ModelAdmin):
             return qs
         return qs.filter(user=request.user)
 
+    def get_form(self, request, obj=None, **kwargs):
+        self.exclude = []
+        if not request.user.is_superuser:
+            self.exclude.extend(['user', ])
+        return super(DailyPlanAdmin, self).get_form(request, obj, **kwargs)
+
+    def save_model(self, request, obj, form, change):
+        if not request.user.is_superuser:
+            obj.user = request.user
+        obj.save()
+
 
 class WeeklyPlanAdmin(admin.ModelAdmin):
-    list_display = ('user', 'admin_names',)
+    list_display = []
     icon = '<i class="material-icons">shopping_cart</i>'
+    exclude = []
 
     def changelist_view(self, request, extra_context=None):
+        self.list_display = []
         if not request.user.is_superuser:
-            self.list_display = ('admin_names',)
+            self.list_display = ['admin_names']
+        else:
+            self.list_display = ['user', 'admin_names']
         return super(WeeklyPlanAdmin, self).changelist_view(request, extra_context)
 
     def get_queryset(self, request):
@@ -39,6 +58,17 @@ class WeeklyPlanAdmin(admin.ModelAdmin):
         if db_field.name == "office_lunch_days":
             kwargs["queryset"] = Day.objects.filter(is_working_day=True)
         return super(WeeklyPlanAdmin, self).formfield_for_manytomany(db_field, request, **kwargs)
+
+    def get_form(self, request, obj=None, **kwargs):
+        self.exclude = []
+        if not request.user.is_superuser:
+            self.exclude.extend(['user', ])
+        return super(WeeklyPlanAdmin, self).get_form(request, obj, **kwargs)
+
+    def save_model(self, request, obj, form, change):
+        if not request.user.is_superuser:
+            obj.user = request.user
+        obj.save()
 
 # Register your models here.
 admin.site.register(DailyPlan, DailyPlanAdmin)
