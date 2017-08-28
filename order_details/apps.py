1af978fa2c6702896b397ef68fe7457953a3f1f8
tm <mashrikt@gmail.com>
2017-08-28 19:16:39 +0600

    User on create, add User's blank weekly plan

--- order_details/apps.py
+++ order_details/apps.py
@@ -4,6 +4,3 @@ from django.apps import AppConfig
 class OrderDetailsConfig(AppConfig):
     name = 'order_details'
     icon = '<i class="material-icons">assignment</i>'
-
-    def ready(self):
-        from order_details.signals import post_save_order
--- reference/signals.py
+++ reference/signals.py
@@ -2,9 +2,13 @@ from django.contrib.auth.models import User, Group
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+from lunch_plan.models import WeeklyPlan
+
 
 @receiver(post_save, sender=User)
-def save_profile(sender, instance, **kwargs):
+def save_profile(sender, instance, created, **kwargs):
+    if created:
+        WeeklyPlan.objects.create(user=instance)
     User.objects.filter(id=instance.id).update(is_staff=True)
     employee_group = Group.objects.filter(name='Employee')
     if employee_group:
